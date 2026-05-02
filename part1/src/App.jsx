ffaaaa2bb594a9191f25c9faeab26a7118c7d4be
mahmoud-b45 <mahmodbarodi02@gmail.com>
2026-05-02 21:55:38 +0300

    part1b
    exercise 1.5 solution

--- part1/src/App.jsx
+++ part1/src/App.jsx
@@ -3,6 +3,7 @@ import Ex1_1 from '../courseinfo/Ex1_1.jsx'
 import Ex1_2 from '../courseinfo/Ex1_2.jsx'
 import Ex1_3 from '../courseinfo/Ex1_3.jsx'
 import Ex1_4 from '../courseinfo/Ex1_4.jsx'
+import Ex1_5 from '../courseinfo/Ex1_5.jsx'
 
 const App = () => {
 
@@ -12,7 +13,8 @@ const App = () => {
       {/* <Ex1_1 /> */}
       {/* <Ex1_2 /> */}
       {/* <Ex1_3 /> */}
-      <Ex1_4 />
+      {/* <Ex1_4 /> */}
+      <Ex1_5 />
     </>
   )
 }
