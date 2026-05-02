65add3c4cea7fb2b858f1aa8a8263458eeab3c11
mahmoud-b45 <mahmodbarodi02@gmail.com>
2026-05-02 21:52:50 +0300

    part1b
    exercise 1.4 solution

---
+++ part1/courseinfo/Ex1_4.jsx
@@ -0,0 +1,47 @@
+const Ex1_4 = () => {
+    const course = 'Half Stack application development'
+    const parts = [
+        {
+            name: 'Fundamentals of React',
+            exercises: 10
+        },
+        {
+            name: 'Using props to pass data',
+            exercises: 7
+        },
+        {
+            name: 'State of a component',
+            exercises: 14
+        }
+    ]
+
+    return (
+        <div>
+            <Header course={course} />
+            <Content parts={parts} />
+            <Total total={parts} />
+        </div>
+    )
+}
+const Header = (props) => <h1>{props.course}</h1>
+
+const Content = ({ parts }) => {
+    return (
+        <>
+            <Part part={parts[0]} />
+            <Part part={parts[1]} />
+            <Part part={parts[2]} />
+        </>
+    )
+}
+
+const Part = ({ part }) => {
+    const { name, exercises } = part
+    return (
+        <p>{name} {exercises}</p>
+    )
+}
+
+const Total = ({ total }) => <p>Number of exercises {total[0].exercises + total[1].exercises + total[2].exercises}</p>
+
+export default Ex1_4
--- part1/src/App.jsx
+++ part1/src/App.jsx
@@ -2,6 +2,7 @@
 import Ex1_1 from '../courseinfo/Ex1_1.jsx'
 import Ex1_2 from '../courseinfo/Ex1_2.jsx'
 import Ex1_3 from '../courseinfo/Ex1_3.jsx'
+import Ex1_4 from '../courseinfo/Ex1_4.jsx'
 
 const App = () => {
 
@@ -10,7 +11,8 @@ const App = () => {
     <>
       {/* <Ex1_1 /> */}
       {/* <Ex1_2 /> */}
-      <Ex1_3 />
+      {/* <Ex1_3 /> */}
+      <Ex1_4 />
     </>
   )
 }
